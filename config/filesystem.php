713de31762daf570edacd59cb3978b78e9977f73
小航 <61196516+xiaohang2020@users.noreply.github.com>
2021-08-18 10:34:55 +0800

    Update filesystem.php

--- config/filesystem.php
+++ config/filesystem.php
@@ -22,29 +22,29 @@ return [
         // 更多的磁盘配置信息
         'aliyun' => [
             'type'         => 'aliyun',
-            'accessId'     => 'LTAI4GKuu2vG78213HzWUgDt7Q4',
-            'accessSecret' => 'ruVlmwtgG6fDG2139urq5Mur9p6FIOD4Y',
+            'accessId'     => 'LTAI4GKuu2vG7821xxx3HzWUgDt7Q4',
+            'accessSecret' => 'ruVlmwtgG6fDG2139urq5Mur9p6xxFIOD4Y',
             'bucket'       => 'yundaohang',
             'endpoint'     => 'oss-cn-beijing.aliyuncs.com',
-            'url'          => 'http://yundaohang.oss-cn-beijing.aliyuncs.com',//不要斜杠结尾，此处为URL地址域名。
+            'url'          => 'http://XXXX.oss-cn-beijing.aliyuncs.com',//不要斜杠结尾，此处为URL地址域名。
         ],
         'qiniu'  => [
             'type'      => 'qiniu',
-            'accessKey' => '8m6_TzcGQjm_VfVh8NYXJ7fdsfCuB6edqzYoCCD7vNYV',
-            'secretKey' => 'lKUVTTmXZxqgFwIAnQ2Hp6sdfBHe4Cw6-VHfaRpYjgC',
+            'accessKey' => '8m6_TzcGQjm_VfVh8NYXJ7fxxxdsfCuB6edqzYoCCD7vNYV',
+            'secretKey' => 'lKUVTTmXZxqgFwIAnQ2Hp6sxxdfBHe4Cw6-VHfaRpYjgC',
             'bucket'    => 'xiaohang-dev',
-            'url'       => 'http://qxmfhkrv7.hn-bkt.clouddn.com',//不要斜杠结尾，此处为URL地址域名。
+            'url'       => 'http://XXXX.hn-bkt.clouddn.com',//不要斜杠结尾，此处为URL地址域名。
         ],
         'qcloud' => [
             'type'       => 'qcloud',
             'region'      => 'ap-guangzhou', //bucket 所属区域 英文
             'appId'      => '125152396223', // 域名中数字部分
-            'secretId'   => 'AKIDtcv2uWPCG7kLiqsdfQLSchn8FFE7vUn5Nde',
+            'secretId'   => 'AKIDtcv2uWPCG7kLiqsdfxxxQLSchn8FFE7vUn5Nde',
             'secretKey'  => 'yindDZzC9qHo9E4mHdsfWUE5h3A7qSq282f',
             'bucket'          => 'yundaohang-1251596223',
             'timeout'         => 60,
             'connect_timeout' => 60,
-            'cdn'             => 'https://yundaohang-125152396223.cos.ap-guangzhou.myqcloud.com',//您的 CDN 域名
+            'cdn'             => 'https://XXXX-125152396223.cos.ap-guangzhou.myqcloud.com',//您的 CDN 域名
             'scheme'          => 'https',
             'read_from_cdn'   => false,
         ]
